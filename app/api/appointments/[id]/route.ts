85254066304de2534a155a832ffa0fdcfc9b30d1
Jose <josevazquez3@gmail.com>
2025-12-07 17:34:20 -0300

    version beta3

--- app/api/appointments/[id]/route.ts
+++ app/api/appointments/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+import { authOptions } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
 import { sendAppointmentCancellation } from '@/lib/email'
 import { formatDate } from '@/lib/utils'
